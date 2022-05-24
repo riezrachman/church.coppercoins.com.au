3a0fabca21582a12941ad67053c5d219a3c0885c
Fariz Rachman Yusuf <farizrachmanyusuf@gmail.com>
2022-05-24 16:01:27 +0700

    refactor: update icon, update dashboard

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,10 +1,7 @@
 <div class="nav-header" style="clip-path: inset(-5px 0px -5px -5px);">
     <div class="brand-logo">
         <a href="{{ url('') }}" draggable="false">
-            <img class="logo-tabib" src="{{ asset('assets/images/icon.png') }}" alt="" draggable="false" style="max-width:35px!important;">
-        </a>
-        <a href="{{ url('') }}" draggable="false">
-            <img class="brand-title mt-0" src="{{ asset('assets/images/logo.png') }}" alt="" draggable="false" style="margin-left:5px;max-width:140px!important;">
+            <img class="brand-title mt-0" src="{{ asset('assets/images/logo_full.png') }}" alt="" draggable="false" style="margin-left:5px;max-width:140px!important;">
         </a>
     </div>
 </div>
@@ -17,37 +14,6 @@
                     <span class="nav-label">Dashboard</span>
                 </a>
             </li>
-            <li class="has-submenu">
-                <a href="{{ url('campaign-management') }}">
-                    <i class="fas fa-clipboard"></i>
-                    <span class="nav-label">Campaign Management</span>
-                </a>
-            </li>
-            <li class="has-submenu">
-                <a href="{{ url('finance') }}">
-                    <i class="fas fa-wallet"></i>
-                    <span class="nav-label">Finance</span>
-                </a>
-            </li>
-            <li class="has-submenu">
-                <a href="{{ url('settings') }}">
-                    <i class="fas fa-user-cog"></i>
-                    <span class="nav-label">Settings</span>
-                </a>
-            </li>
-            <li class="has-submenu">
-                <a href="{{ url('materials') }}">
-                    <i class="fas fa-folder-open"></i>
-                    <span class="nav-label">Materials</span>
-                </a>
-            </li>
         </ul>
     </nav>
-    <div class="sidebar-widgets">
-        <div class="top-sidebar box-shadow mx-25 m-b-30 p-b-20 text-center">
-            <a draggable="false">
-                <img src="{{ asset('assets/images/icon_fade.png') }}" class="side-img" alt="img" style="right:0;position:absolute;bottom:0;z-index:-1;" draggable="false">
-            </a>
-        </div>
-    </div>
 </aside>
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -34,20 +34,6 @@
 <div class="row">
     <div class="col-md-12">
         <div class="card custom-card">
-            <div class="card-body" style="padding: 15px;" id="div-charity-detail">
-                <center>
-                    <div class="spinner-border spinner-border-sm text-primary" role="status">
-                        <span class="sr-only">Loading...</span>
-                    </div>
-                </center>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-md-12">
-        <div class="card custom-card">
             <div class="card-body" style="padding: 15px;">
                 <div class="d-flex justify-content-between align-items-baseline">
                     <h5 class="card-title" style="margin-bottom: 0px;">Donations</h5>
@@ -79,118 +65,7 @@
 
 <script>
 
-    $(function () {
-
-        getCardCounter();
-        getCharityDetail();
-        getDonorData();
-
-    });
-
-    const getCardCounter = async () => {
-
-        var res = await axios.get(`/api/dashboard/counter`, {
-            headers: {
-                "Authorization": `Bearer ${apiToken}`,
-            },
-        });
-
-        if (res.data.status == true) {
-
-            var data = res.data.data;
-
-            $('#total-donated').text(data.total_donated);
-            $('#current-active-balance').text(data.current_active_balance);
-            $('#watchlist-count').text(data.watchlist_count);
-
-        }
-
-    }
-
-    const getCharityDetail = async () => {
-
-        var res = await axios.get(`/api/auth/profile`, {
-            headers: {
-                "Authorization": `Bearer ${apiToken}`,
-            },
-        });
-
-        if (res.data.status == true) {
-
-            var data = res.data.data;
-
-            $('#div-charity-detail').html(`
-                <div class="custom-card-header mb-3">
-                    <h5 class="card-title" style="margin-bottom: 0px;">${data.organization.name}</h5>
-                </div>
-                <div class="d-flex px-3">
-                    <img src="{{ asset('assets/images/default_image.png') }}" alt="" style="border-radius: 50%;" width="100px" height="100px">
-                    <div class="d-flex flex-column align-items-stretch" style="margin-left: 40px;">
-                        <p>
-                            <span class="text-muted">ABN</span><br/>
-                            <b>${data.organization.business_phone}</b>
-                        </p>
-                        <p>
-                            <span class="text-muted">Category</span><br/>
-                            <b>${data.organization.categories[0].ch_category_name}</b>
-                        </p>
-                    </div>
-                    <div class="d-flex flex-column align-items-stretch" style="margin-left: 90px;">
-                        <p>
-                            <span class="text-muted">Email</span><br/>
-                            <b>${data.email}</b>
-                        </p>
-                        <p>
-                            <span class="text-muted">Phone</span><br/>
-                            <b>${data.organization.phone}</b>
-                        </p>
-                    </div>
-                </div>
-            `);
-
-        }
-
-
-    }
-
-    const getDonorData = () => {
-
-        var html = `
-            <tr>
-                <td colspan="4"><center>No Data</center></td>
-            </tr>
-        `;
-
-        // html = `
-        //     <tr>
-        //         <td>05:00 PM, 04-10-2021</td>
-        //         <td>$ 65 AUD</td>
-        //         <td>Rebecca Foster</td>
-        //         <td>rebeccafoster@gmail.com</td>
-        //     </tr>
-        //     <tr>
-        //         <td>05:00 PM, 04-10-2021</td>
-        //         <td>$ 65 AUD</td>
-        //         <td>Rebecca Foster</td>
-        //         <td>rebeccafoster@gmail.com</td>
-        //     </tr>
-        //     <tr>
-        //         <td>05:00 PM, 04-10-2021</td>
-        //         <td>$ 65 AUD</td>
-        //         <td>Rebecca Foster</td>
-        //         <td>rebeccafoster@gmail.com</td>
-        //     </tr>
-        // `;
-
-        $('#table-donor').find('tbody').html(html);
-
-    }
-
-    const exportDonorData = () => {
-
-        // TODO
-
-    }
+    
 
 </script>
 
